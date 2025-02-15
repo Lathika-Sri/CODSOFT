959ce4f961488b161b04c49c2ea9ad0af3a9528b
Lathika-Sri <lathikasribalamurugan6@gmail.com>
2025-02-15 05:32:00 +0530

    Script file:portfolio

---
+++ script2.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.navbar a');
+
+    sections.forEach(section => {
+        const top = window.scrollY;
+        const offset = section.offsetTop - 100;
+        const height = section.offsetHeight;
+        const id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('.navbar a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+});
